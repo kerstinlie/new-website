3d6dad6e313bf96885bfb434045504bc5cfeed9f
Ketty <bettina.klostermann@inbetween.de>
2026-08-10 12:25:24 +0200

    Sanity CLI-Konfiguration für Studio-Deploy ergänzen

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli';
+
+export default defineCliConfig({
+  api: {
+    projectId: 'gev7dohx',
+    dataset: 'production',
+  },
+  studioHost: 'inbetween-website',
+});
